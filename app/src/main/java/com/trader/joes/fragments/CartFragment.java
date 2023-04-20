2d8960875089d6d40148266e2408917a56b3e18c
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-04-20 11:04:16 -0400

    Functional Cart headers

--- app/src/main/java/com/trader/joes/fragments/CartFragment.java
+++ app/src/main/java/com/trader/joes/fragments/CartFragment.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.trader.joes.R;
 import com.trader.joes.adapter.CartListAdapter;
+import com.trader.joes.model.CartItem;
 import com.trader.joes.model.User;
+import com.trader.joes.service.AuthService;
 import com.trader.joes.service.UserDataMaintenanceService;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class CartFragment extends Fragment {
 
+    private TextView mTotalItems;
+    private TextView mSubtotalAmt;
     private CartListAdapter mCartListAdapter;
     private RecyclerView mCartListView;
     private UserDataMaintenanceService userDataMaintenanceService;
@@ -25,15 +34,43 @@ public class CartFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
 
+        mTotalItems = view.findViewById(R.id.total_items);
+        mSubtotalAmt = view.findViewById(R.id.subtotal_amount);
         mCartListView = view.findViewById(R.id.cartRecyclerView);
 
         userDataMaintenanceService = new UserDataMaintenanceService();
 
-        User currentUser = userDataMaintenanceService.getCurrentUserData();
-        mCartListAdapter = new CartListAdapter(currentUser.getCartItems());
-        
-        mCartListView.setAdapter(mCartListAdapter);
+        Consumer<User> successConsumer = new Consumer<User>() {
+            @Override
+            public void accept(User user) {
+                List<CartItem> cartItems = user.getCartItems();
+                updateCartHeaders(cartItems);
+
+                mCartListAdapter = new CartListAdapter(cartItems);
+                mCartListView.setAdapter(mCartListAdapter);
+            }
+        };
+        Consumer<String> failureConsumer = new Consumer<String>() {
+            @Override
+            public void accept(String unused) {
+                Toast.makeText(getActivity(), "DB connection failed!", Toast.LENGTH_SHORT).show();
+            }
+        };
+        userDataMaintenanceService.getCurrentUserData(new AuthService().getCurrentUser().getUid(), successConsumer, failureConsumer);
 
         return view;
+    }
+
+    private void updateCartHeaders(List<CartItem> cartItems){
+        int totalItems = 0;
+        float subtotalAmt = 0;
+
+        for(CartItem item: cartItems) {
+            totalItems += item.getQty();
+            subtotalAmt += Float.parseFloat(item.getPrice())*item.getQty();
+        }
+
+        mTotalItems.setText(String.valueOf(totalItems) + " items");
+        mSubtotalAmt.setText("$" + String.valueOf(subtotalAmt));
     }
 }
