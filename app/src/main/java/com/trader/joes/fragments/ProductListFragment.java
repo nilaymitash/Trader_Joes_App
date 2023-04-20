c67582bd068bda3814c79a3c67ad96aa116ead1c
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-04-20 13:17:24 -0400

    material floating button

--- app/src/main/java/com/trader/joes/fragments/ProductListFragment.java
+++ app/src/main/java/com/trader/joes/fragments/ProductListFragment.java
@@ -14,6 +14,7 @@ import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseError;
 import com.trader.joes.R;
 import com.trader.joes.adapter.ProductListAdapter;
@@ -30,7 +31,8 @@ public class ProductListFragment extends Fragment {
     private SearchView mSearchView;
     private ProgressBar mProgressBar;
     private RecyclerView mProductListView;
-    private Button mViewCartBtn;
+    //private Button mViewCartBtn;
+    private FloatingActionButton mViewCartBtn;
     private ProductListAdapter mProductAdapter;
     private ProductRetrievalService productRetrievalService;
     private List<Product> allProducts = new ArrayList<>();
