21b98ae76d34cd083c5faaed316251837f0ae72c
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-04-22 12:48:04 -0400

    Cleanup

--- app/src/main/java/com/trader/joes/fragments/BarcodeScannerFragment.java
+++ app/src/main/java/com/trader/joes/fragments/BarcodeScannerFragment.java
@@ -2,8 +2,10 @@ package com.trader.joes.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,12 +13,17 @@ import com.trader.joes.R;
 
 public class BarcodeScannerFragment extends Fragment {
 
+    private SurfaceView surfaceView;
+    private TextView barcodeText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_barcode_scanner, container, false);
 
         //initialize UI components
+        surfaceView = view.findViewById(R.id.surfaceView);
+        barcodeText = view.findViewById(R.id.barcode_value);
 
         return view;
     }
