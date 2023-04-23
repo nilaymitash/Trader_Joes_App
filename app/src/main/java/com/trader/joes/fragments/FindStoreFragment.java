bcc3af5b3bfb22ee915313c126d948cbe06b9672
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-04-22 23:58:43 -0400

    Google maps integration

--- app/src/main/java/com/trader/joes/fragments/FindStoreFragment.java
+++ app/src/main/java/com/trader/joes/fragments/FindStoreFragment.java
@@ -2,15 +2,23 @@ package com.trader.joes.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.trader.joes.R;
 
-public class FindStoreFragment extends Fragment {
+public class FindStoreFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,7 +26,22 @@ public class FindStoreFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_find_store, container, false);
 
         //initialize UI components
-
+        // Initialize map fragment
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
