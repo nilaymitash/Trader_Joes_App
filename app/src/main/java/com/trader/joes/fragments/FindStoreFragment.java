b8c0e06d1b0babaad28926df19fcada9061bf637
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-04-25 01:01:12 -0400

    Rudimentary POST call

--- app/src/main/java/com/trader/joes/fragments/FindStoreFragment.java
+++ app/src/main/java/com/trader/joes/fragments/FindStoreFragment.java
@@ -1,5 +1,14 @@
 package com.trader.joes.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,19 +23,26 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.textfield.TextInputEditText;
 import com.trader.joes.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class FindStoreFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private Location currentLocation;
+    private LocationManager locationManager;
+    private TextInputEditText mZipcodeInput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,10 +50,19 @@ public class FindStoreFragment extends Fragment implements OnMapReadyCallback {
         View view = inflater.inflate(R.layout.fragment_find_store, container, false);
 
         //initialize UI components
+        mZipcodeInput = view.findViewById(R.id.zipcode_input);
+
+
         // Initialize map fragment
         /*SupportMapFragment mapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);*/
+
+        //initialize Location Manager
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        getCoordinates();
+        //String zipcode = getZipCode(coordinates);
 
         //move this to on click of a button: https://code.tutsplus.com/tutorials/android-from-scratch-using-rest-apis--cms-27117
         new FindStoreTask().execute();
@@ -55,6 +80,51 @@ public class FindStoreFragment extends Fragment implements OnMapReadyCallback {
                 .position(sydney)
                 .title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private boolean isLocationPermissionGranted() {
+        if(getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && getActivity().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            getActivity().requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+            return false;
+        }
+
+        return true;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCoordinates() {
+        Coordinates coordinates = new Coordinates();
+        boolean hasGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        LocationListener gpsLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                coordinates.setLongitude(location.getLongitude());
+                coordinates.setLatitude(location.getLatitude());
+                String zipcode = getZipCode(coordinates);
+                if(mZipcodeInput.getText() == null || mZipcodeInput.getText().toString().equals("")) {
+                    mZipcodeInput.setText(zipcode);
+                }
+            }
+        };
+
+        if(isLocationPermissionGranted() && hasGPS) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, gpsLocationListener);
+        }
+    }
+
+
+    private String getZipCode(Coordinates coordinates) {
+        Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(coordinates.getLatitude(), coordinates.getLongitude(), 1);
+            return addresses.get(0).getPostalCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "19078";
+        }
     }
 
     private class FindStoreTask extends AsyncTask<Void, Void, String> {
@@ -138,6 +208,27 @@ public class FindStoreFragment extends Fragment implements OnMapReadyCallback {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private class Coordinates {
+        private double latitude;
+        private double longitude;
+
+        public double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(double latitude) {
+            this.latitude = latitude;
+        }
+
+        public double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(double longitude) {
+            this.longitude = longitude;
         }
     }
 }
