62a8a511ad0558e462857058846fac1b58b2cceb
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-04-27 17:22:37 -0400

    order history finished

--- app/src/main/java/com/trader/joes/adapter/OrderHistoryListAdapter.java
+++ app/src/main/java/com/trader/joes/adapter/OrderHistoryListAdapter.java
@@ -6,23 +6,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.trader.joes.R;
 import com.trader.joes.model.CartItem;
+import com.trader.joes.model.Product;
 import com.trader.joes.model.Transaction;
+import com.trader.joes.service.ProductRetrievalService;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OrderHistoryListAdapter extends BaseExpandableListAdapter {
 
     private Context context;
     private List<Transaction> transactionList;
+    private Map<String, Product> productMap;
 
     public OrderHistoryListAdapter(Context context, List<Transaction> transactionList) {
         this.context = context;
         this.transactionList = transactionList;
+        this.productMap = new ProductRetrievalService().getAllProductsMap();
     }
 
     @Override
@@ -37,7 +43,7 @@ public class OrderHistoryListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public Object getGroup(int listPosition) {
-        return this.transactionList.get(listPosition).getTransactionId();
+        return this.transactionList.get(listPosition);
     }
 
     @Override
@@ -64,16 +70,26 @@ public class OrderHistoryListAdapter extends BaseExpandableListAdapter {
     @Override
     public View getGroupView(int listPosition, boolean isExpanded,
                              View convertView, ViewGroup parent) {
-        String listTitle = (String) getGroup(listPosition);
+        Transaction transaction = (Transaction) getGroup(listPosition);
         if (convertView == null) {
             LayoutInflater layoutInflater = (LayoutInflater) this.context.
                     getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = layoutInflater.inflate(R.layout.order_list_group, null);
         }
-        TextView listTitleTextView = (TextView) convertView
-                .findViewById(R.id.order_group_title);
-        listTitleTextView.setTypeface(null, Typeface.BOLD);
-        listTitleTextView.setText(listTitle);
+        TextView dateView = (TextView) convertView
+                .findViewById(R.id.order_date);
+        TextView amountView = (TextView) convertView
+                .findViewById(R.id.order_amount);
+        TextView idView = (TextView) convertView
+                .findViewById(R.id.order_id);
+
+        dateView.setTypeface(null, Typeface.BOLD);
+        amountView.setTypeface(null, Typeface.BOLD);
+        idView.setTypeface(null, Typeface.BOLD);
+
+        dateView.setText(transaction.getTransactionDate());
+        amountView.setText(transaction.getTransactionAmt());
+        idView.setText(transaction.getTransactionId());
         return convertView;
     }
 
@@ -81,16 +97,22 @@ public class OrderHistoryListAdapter extends BaseExpandableListAdapter {
     public View getChildView(int listPosition, final int expandedListPosition,
                              boolean isLastChild, View convertView, ViewGroup parent) {
         CartItem item = (CartItem) getChild(listPosition, expandedListPosition);
-        String expandedListText = item.getProductSku() + " | " + item.getQty() + " | " + item.getPrice();
 
         if (convertView == null) {
             LayoutInflater layoutInflater = (LayoutInflater) this.context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = layoutInflater.inflate(R.layout.order_list_item, null);
         }
-        TextView expandedListTextView = (TextView) convertView
-                .findViewById(R.id.expanded_order_list_item);
-        expandedListTextView.setText(expandedListText);
+
+        ImageView productImgView = convertView.findViewById(R.id.order_history_product_img);
+        TextView productNameView = convertView.findViewById(R.id.order_history_product_name);
+        TextView qtyView = convertView.findViewById(R.id.order_history_product_qty);
+        TextView priceView = convertView.findViewById(R.id.order_history_product_price);
+
+        Picasso.get().load(this.productMap.get(item.getProductSku()).getImgURL()).into(productImgView);
+        productNameView.setText(this.productMap.get(item.getProductSku()).getProductName());
+        qtyView.setText(String.valueOf(item.getQty()));
+        priceView.setText("$" + this.productMap.get(item.getProductSku()).getPrice());
         return convertView;
     }
 
